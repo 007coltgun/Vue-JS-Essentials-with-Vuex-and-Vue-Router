de92041df4ecb3ea6846d212c9d26005412083a5
TonyChen <tonychenn@gmail.com>
2018-05-14 00:14:05 +0800

    ch3-14_Searching Youtube

--- video-browser/src/App.vue
+++ video-browser/src/App.vue
@@ -5,8 +5,10 @@
 </template>
 
 <script>
+import axios from "axios";
 import SearchBar from "./components/SearchBar";
 const API_KEY = 'AIzaSyCrS_fgQDO6RWv7AdcxIde89t7It8IOcvc';
+const YOUTUBE_V3_URL = 'https://www.googleapis.com/youtube/v3/search';
 export default {
   name: "App",
   components: {
@@ -14,7 +16,15 @@ export default {
   },
   methods: {
     onTermChange(searchTerm) {
-      console.log(searchTerm);
+      axios.get(YOUTUBE_V3_URL, {
+        params: {
+          key: API_KEY,
+          type: 'video',
+          part: 'snippet',
+          q: searchTerm
+        }
+      })
+      .then(response => console.log(response))
     }
   }
 };
