71c007fd025e3f06820ea9efcc9cc9aa100f5121
TonyChen <tonychenn@gmail.com>
2018-05-14 23:34:59 +0800

    ch3-25_finish VideoList and VideoListItem

--- video-browser/src/App.vue
+++ video-browser/src/App.vue
@@ -1,18 +1,27 @@
 <template>
   <div id="app">
     <SearchBar @termChange="onTermChange"></SearchBar>
+    <VideoList :videos="videos"></VideoList>
   </div>
 </template>
 
 <script>
 import axios from "axios";
 import SearchBar from "./components/SearchBar";
+import VideoList from './components/VideoList';
+
 const API_KEY = 'AIzaSyCrS_fgQDO6RWv7AdcxIde89t7It8IOcvc';
 const YOUTUBE_V3_URL = 'https://www.googleapis.com/youtube/v3/search';
 export default {
   name: "App",
   components: {
-    SearchBar: SearchBar
+    SearchBar: SearchBar,
+    VideoList: VideoList
+  },
+  data() {
+    return {
+      videos: []
+    }
   },
   methods: {
     onTermChange(searchTerm) {
@@ -24,7 +33,9 @@ export default {
           q: searchTerm
         }
       })
-      .then(response => console.log(response))
+      .then(response => {
+        this.videos = response.data.items;
+      })
     }
   }
 };
---
+++ video-browser/src/components/VideoList.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul>
+    <VideoListItem
+     v-for="video in videos"
+     :key="video.id.videoId"
+     :video="video">
+    </VideoListItem>
+  </ul>
+</template>
+
+<script>
+import VideoListItem from './VideoListItem';
+
+export default {
+  name: "VideoList",
+  props: {
+    videos: Array
+  },
+  components: {
+    VideoListItem: VideoListItem
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
