a2404528e91ded2b65d0ded470f7e2b68072f0ff
TonyChen <tonychenn@gmail.com>
2018-05-15 00:26:39 +0800

    ch3-30_用bootstrap對初步的VideoList, VideoListItem樣式

--- video-browser/src/components/VideoListItem.vue
+++ video-browser/src/components/VideoListItem.vue
@@ -1,13 +1,22 @@
 <template>
-  <li>
-    {{video.snippet.title}}
+  <li class="list-group-item">
+    <img :src="thumbnailUrl" alt="">
+    {{videoTitle}}
   </li>
 </template>
 
 <script>
 export default {
   name: "VideoListItem",
-  props: ['video']
+  props: ['video'],
+  computed: {
+    videoTitle() {
+      return this.video.snippet.title;
+    },
+    thumbnailUrl() {
+      return this.video.snippet.thumbnails.default.url
+    }
+  }
 };
 </script>
 
