254c5ed93f802d43008872b36a5eabc7c475b9e3
TonyChen <tonychenn@gmail.com>
2018-05-21 10:18:33 +0800

    可上傳照片到imgur並顯示在ImageList

--- images/src/components/UploadForm.vue
+++ images/src/components/UploadForm.vue
@@ -1,15 +1,39 @@
 <template>
-  <div>
-    Upload Images
+  <div class="dropper">
+    <input 
+     type="file" 
+     @change="uploadImages($event.target.files)"
+     multiple
+     accept="image/*">
+    <span>Drag file her!</span>
   </div>
 </template>
 
 <script>
+import { mapActions} from 'vuex';
 export default {
-  name: 'UploadForm'
+  name: 'UploadForm',
+  methods: mapActions(['uploadImages'])
 }
 </script>
 
-<style>
-
+<style scoped>
+.dropper {
+  height: 30vh;
+  border: 1px dashed black;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+}
+.dropper:hover {
+  background-color: #eee;
+}
+input {
+  width: 100%;
+  height: 30vh;
+  opacity: 0;
+  position: absolute;
+}
 </style>
--- images/src/store/modules/images.js
+++ images/src/store/modules/images.js
@@ -1,24 +1,35 @@
 import api from '../../api/imgur';
+import { router } from '../../main';
 
 const state = {
   allImages: []
 }
 
 const getters = {
+  allImages: state => state.allImages
 }
 
 const mutations = {
   setImages: (state, images) => {
-    state.images = images;
+    state.allImages = images;
   }
-
 }
 
 const actions = {
-  async fetchImages({rootState}) {
+  async fetchImages({rootState, commit}) {
     const {token} = rootState.auth;
     const response = await api.fetchImages(token);
-    console.log(response);
+    commit('setImages', response.data.data);
+  },
+  async uploadImages({rootState}, images) {
+    // Get access toke
+    const {token} = rootState.auth;
+
+    // call our API module to do the upload
+    await api.uploadImages(images, token);
+
+    // Redirect our user to ImageList
+    router.push('/');
   }
 }
 
